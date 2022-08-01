cb6c18c6f614351675b96cd655a7d389c5d421e6
priyanka.date <priyanka.date@ojassoft.in>
2022-08-01 17:02:41 +0530

    fetchuser info

--- my-app-github-master-master/src/App.js
+++ my-app-github-master-master/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import UserDetailsComponent from "./Component/UserDetailsComponent";
-import ImageComponent from "./Component/ImageComponent";
+
 
 
 function App() {
    return (
         <div>
          <UserDetailsComponent/>
-         <ImageComponent/>
+         
        
          </div>
    );
--- my-app-github-master-master/src/Component/UserDetailsComponent.js
+++ my-app-github-master-master/src/Component/UserDetailsComponent.js
@@ -1,27 +1,48 @@
+import React ,{useState} from 'react'
+import axios from 'axios'
 
-const UserDetailsComponent=()=>{
+function UserDetailsComponent() {
+
+    const [user,setUser]= useState([])
+    const fetchUsers=()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((response)=>{
+           setUser(response.data)
+          // console.log(response);
+        })
+        }
     
-  let url="https://jsonplaceholder.typicode.com/users";
-  const FetchData=()=>{
-  fetch(url)
-  .then((r)=>{
-      return r.json();
-  })
-  .then((result)=>{
-      const newResult = result.filter(function(e){
-          return e.id<3
-      });
-     console.log(newResult);
-   
-  });
+  return (
+    <>
+    <div>
+    <button className='btn btn-primary' onClick={fetchUsers}>FetchUsers</button>
+    </div>
+   <div className='container'>
+    <div className='row'> 
+       {  
+        user.map((value)=>{
+            return(
+                <div className='col-4'>
+                <div className="card" style={{width: "1 8rem"}}>
+          <img src="https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h1 className="card-title">{value.name}</h1>
+            <p className="card-text">Email : {value.email}</p>
+            <p className="card-text">Phone :{value.phone}</p>
+            <p className="card-text">Website : {value.website}</p>
+            <p className="card-text">Website : {value.address.street}, {value.address.suite}, {value.address.city}, {value.address.zipcode}</p>
+            <p className="card-text">company : {value.company.name}</p>
+          </div>
+        </div>
+                </div>
+            )
+        })
+       }
+    </div>
+   </div>
 
-};
-FetchData();
-  return(
-<div>
-  <p>User Information</p>
- 
-</div>
-  );
-};
-export default  UserDetailsComponent;
+    </>
+  )
+  }
+
+export default UserDetailsComponent
