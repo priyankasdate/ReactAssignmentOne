ffabf2eedba3d9300c348d853bcb310e922a9d2a
priyanka.date <priyanka.date@ojassoft.in>
2022-08-08 11:10:18 +0530

    Added css And userlistcomponent

--- my-app-github-master-master/src/Component/UserDetailsComponent.js
+++ my-app-github-master-master/src/Component/UserDetailsComponent.js
@@ -1,57 +1,50 @@
-import React, { useEffect ,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
- import "./UserComponent.css";
+import "./UserComponent.css"
+import UserListComponent from './UserListComponent';
 
 
 function UserDetailsComponent() {
 
   const [namesArray, setNamesArray] = useState([]);
   const [user, setUser] = useState([]);
-  useEffect ( () => {
+ 
+  useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         setUser(response.data);
         let namesTempArray = [];
         for (let i = 0; i < user.length; i++) {
           namesTempArray.push(user[i].name);
-         
+
         }
         setNamesArray(namesTempArray);
-         console.log(namesArray);
+        console.log(namesArray);
       })
-  },[])
+  }, [])
 
   return (
     <>
       {
         <div >
-        {user.map((value , key)=>{
-          // namesArray.push(value.name);
-          return(
-            <div className='tbldata'>
-            <table>
-            <tr key={key} className="userdata">
-              <tr><th>Name : {value.name}</th></tr>  
+          {user.map((value, key) => {
+            
+            let url = "https://avatars.dicebear.com/v2/avataaars/{{"+value.name+"}}.svg?options[mood][]=" 
+            return (
+              <UserListComponent name={value.name}
+                email={value.email}
+                phone={value.phone}
+                company={value.company}
+                website={value.website}
+                address={value.address}
+              url={url }
+              />
+            )
+          })}
 
-             <tr> <td>Email:  {value.email}</td></tr>
+        </div>
+      }
 
-             <tr> <td>Phone: {value.phone}</td></tr>
-
-             <tr> <td>Company: {value.company.name}</td></tr>
-
-             <tr> <td>Website: {value.website}</td></tr>
-              
-             <tr> <td>Address:{value.address.street},{value.address.suite},{value.address.city},{value.address.zipcode}</td></tr>
-            </tr>
-            </table>
-            </div>
-          )
-        })}
-      
-      </div>
-      
-    }
-      
 
     </>
   )
--- my-app-github-master-master/src/Component/UserListComponent.js
+++ my-app-github-master-master/src/Component/UserListComponent.js
@@ -1,78 +1,31 @@
+import React from 'react'
+import "./UserComponent.css"
+function UserListComponent(props) {
+    console.log(props.url)
+    return (
+        <div className='container'>
+            <table className="userdata">
+                <tr>
+                    
+                        <td className='imgurl'>
+                            <img src= {props.url}></img>
+                        </td>
+                   <td>
+                    <tr><th> {props.name}</th></tr>
 
-import React, { useEffect ,useState } from 'react'
-import axios from 'axios'
-import ImageComp from './ImageComp'
+                    <tr> <td>Email: {props.email}</td></tr>
+                    
+                    <tr> <td>Phone: {props.phone}</td></tr>
 
-function UserDetailsComp() {
+                    <tr> <td>Company: {props.company.name}</td></tr>
 
-  const [user, setUser] = useState([])
-  useEffect ( () => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        setUser(response.data)
-        // console.log(response);
-      })
-  })
+                    <tr> <td>Website: {props.website}</td></tr>
 
-  return (
-    <>
-      {
-        <div className='user'>
-      <table>
-       
-        {user.map((value , key)=>{
-          return(
-            <tr key={key}>
-              <th>{value.name}</th>
-              <td>{value.email}</td>
-              <td>{value.phone}</td>
-              <td>{value.company.name}</td>
-              <td>{value.website}</td>
-              <td>{value.address.street},{value.address.suite},{value.address.city},{value.address.zipcode}</td>
-            </tr>
-          )
-        })}
-      </table>
-      </div>
-      /* <div className='container'>
-        <div className='row'>
-          {
-            user.map((value) => {
-              return (
-
-                <div className="card mb-3" style={{ width: "18rem" }}>
-                  <div className="row g-0">
-                    <div className="col-md-4">
-                      <img src="https://avatars.dicebear.com/v2/avataaars/{{username}}.svg?options[mood][]=happy" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="col-md-8">
-                      <div className="card-body">
-                        <h1 >{value.name}</h1>
-                        <p >Email : {value.email}</p>
-                        <p >Phone :{value.phone}</p>
-                        <p >Company : {value.company.name}</p>
-                        <p >Website : {value.website}</p>
-                        <p >Address : {value.address.street},{value.address.suite},{value.address.city},{value.address.zipcode}</p>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )
-            })
-          }
+                    <tr> <td>Address: {props.address.street},{props.address.suite},{props.address.city},{props.address.zipcode}</td></tr>
+                    </td> </tr>
+            </table>
         </div>
-      </div> */}
-
-    </>
-  )
+    )
 }
 
-export default UserDetailsComp
-
-
-
-
-
-
-
+export default UserListComponent
